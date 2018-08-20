254f256c7c5105a24ae67667fa33ccf7e4b1c486
Guilherme Neres <guilherme.neres.henrique@gmail.com>
2018-08-20 01:21:28 -0300

    Learning about Jpa Managed state  and the Find method to get a specific entity

---
+++ src/br/com/caelum/financas/teste/TesteBuscaConta.java
@@ -0,0 +1,25 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.model.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteBuscaConta {
+	
+	public static void main(String[] args) {
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+		em.getTransaction().begin();
+		
+		Conta conta = em.find(Conta.class, 1);
+		
+		System.out.println("O titular da conta e: " + conta.getTitular());
+		
+		em.getTransaction().commit();
+		
+		em.close();
+		
+	}
+
+}
